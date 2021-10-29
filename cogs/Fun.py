a81f8bf895b0f62ab82ac7008abbd7e59ca23a9b
SadlerJ <28812304+sadlerjames@users.noreply.github.com>
2021-10-30 00:08:35 +0100

    Add more word listeners

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -12,7 +12,7 @@ class Fun(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    #meme
+    #listen for words
     @commands.Cog.listener()
     async def on_message(self, message):
 
@@ -21,24 +21,31 @@ class Fun(commands.Cog):
 
         if ("meme" or "Meme" or "MEME") in message.content:
             await message.channel.send("Did someone say meme?")
-            #send meme inside embed message
             embed = discord.Embed(title="")
             embed.set_image(url="https://c.tenor.com/jItxRf-KWosAAAAC/jeffrey-dean-morgan-smile.gif")
             await message.channel.send(embed=embed)
 
-    #spider man
-    @commands.Cog.listener()
-    async def on_message(self, message):
-
-        if message.author == self.client.user:
-            return
-
-        print(message.content)
-
-        if ("spider-man" or "peter-parker" or "peter parker" or "spiderman" or "spider man" or "tobey maguire" or "tobey" or "tobeymaguire") in message.content:
+        if ("spiderman" or "peter-parker" or "peter parker" or "spider-man" or "spider man" or "tobey maguire" or "tobey" or "tobeymaguire") in message.content:
             embed = discord.Embed(title="")
             embed.set_image(url="https://c.tenor.com/P8VsDwHZreYAAAAd/tobey-maguire-spider-man.gif")
             await message.channel.send(embed=embed)
+
+        if ("fuck") in message.content:
+            embed = discord.Embed(title="")
+            embed.set_image(url="https://c.tenor.com/Rt5-pEJHt04AAAAC/fuck-damn.gif")
+            await message.channel.send(embed=embed)
+
+        if ("game") in message.content:
+            embed = discord.Embed(title="")
+            embed.set_image(url="https://c.tenor.com/xtp2N6fg2SwAAAAC/stickergiant-game-time.gif")
+            await message.channel.send(embed=embed)
+        
+        if ("sorry") in message.content:
+            embed = discord.Embed(title="")
+            embed.set_image(url="https://c.tenor.com/B1ooFF-GRUMAAAAd/no-apology-necessary-travis.gif")
+            await message.channel.send(embed=embed)
+
+        
 
 
 def setup(client):
